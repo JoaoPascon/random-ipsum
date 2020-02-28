7ac94cd1c1e8f0873c979f0c07b0a0dfcb2b4127
João Pascon <joaonatan93@gmail.com>
2020-02-27 22:18:39 -0300

    Deploy

---
+++ build/precache-manifest.a77ff50bc51e772cce18478dc3c1a87d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4583f5822e923a8e7c697455434fc00a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b491338d0c590de04fc1",
+    "url": "/static/css/main.358297b8.chunk.css"
+  },
+  {
+    "revision": "b5e376a01bba7b5f5619",
+    "url": "/static/js/2.d6b499c5.chunk.js"
+  },
+  {
+    "revision": "b491338d0c590de04fc1",
+    "url": "/static/js/main.867d1db2.chunk.js"
+  },
+  {
+    "revision": "0aef192f74109484e109",
+    "url": "/static/js/runtime-main.2a77f20a.js"
+  },
+  {
+    "revision": "83be36bf08bbaf6fd5fa4fffcfe219f2",
+    "url": "/static/media/craque_neto.83be36bf.jpg"
+  },
+  {
+    "revision": "a3bfd07c8221c81a4574316903d57de2",
+    "url": "/static/media/fausto_silva.a3bfd07c.jpg"
+  },
+  {
+    "revision": "4d0a9fd3316fca96d6509951d8cfbd0b",
+    "url": "/static/media/hodor.4d0a9fd3.jpg"
+  },
+  {
+    "revision": "30c0cc05aec6570ee99c3a98dbc20472",
+    "url": "/static/media/jacquin.30c0cc05.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
